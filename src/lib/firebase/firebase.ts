654cb7130bc089ea0bc60b0ed8c0fb99ee8fdeb5
Evan S Jones <esj.ejones@gmail.com>
2025-04-04 14:10:08 -0400

    changed process to import.meta in firebase.ts, added future-proofing for BrowswerRouter in main.tsx, and disabled sourcemap errors in vite.config.ts

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -13,17 +13,17 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
 
-  apiKey: process.env.VITE_APP_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
 
-  authDomain: process.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
 
-  projectId: process.env.VITE_APP_FIREBASE_PROJECT_ID,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
 
-  storageBucket: process.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
 
-  messagingSenderId: process.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
 
-  appId: process.env.VITE_APP_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID
 
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <App />
         </BrowserRouter>
       </QueryClientProvider>
