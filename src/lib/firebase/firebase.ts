adc9af339d5d78f4c71d6c32bbaf4aed7bdd4beb
Evan S Jones <esj.ejones@gmail.com>
2025-04-08 13:59:21 -0400

    added quotes around firebaseConfig values...and I think it is working

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -13,17 +13,17 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
 
-  apiKey: process.env.VITE_APP_FIREBASE_API_KEY,
+  apiKey: "process.env.VITE_APP_FIREBASE_API_KEY",
 
-  authDomain: process.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  authDomain: "process.env.VITE_APP_FIREBASE_AUTH_DOMAIN",
 
-  projectId: process.env.VITE_APP_FIREBASE_PROJECT_ID,
+  projectId: "process.env.VITE_APP_FIREBASE_PROJECT_ID",
 
-  storageBucket: process.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  storageBucket: "process.env.VITE_APP_FIREBASE_STORAGE_BUCKET",
 
-  messagingSenderId: process.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: "process.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID",
 
-  appId: process.env.VITE_APP_FIREBASE_APP_ID
+  appId: "process.env.VITE_APP_FIREBASE_APP_ID"
 
 };
 
